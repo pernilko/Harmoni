ad887fc536c7fa45b81022e8e22644bb522b73ef
Brigittb <brigittb@stud.ntnu.no>
2020-01-08 22:40:10 +0100

    Org Container

--- client/src/Containers/Event/Components/registrationFormEvent.js
+++
@@ -1,8 +0,0 @@
-//@flow
-
-import * as React from 'react';
-import { Component } from "react-simplified";
-
-export class RegistrationForm extends Component {
-
-}
--- client/src/Containers/Organization/Components/registerOrgForm.js
+++ client/src/Containers/Organization/Components/registerOrgForm.js
@@ -1,1 +1,36 @@
-g
+//@flow
+
+
+import * as React from 'react';
+import {Organization} from '../../../services/OrganizationService';
+
+
+export class RegOrganization extends Component {
+  organization: Organization = new Organization();
+
+  render(){
+    return (
+      <div>
+        <h2 className="card-header">Register a new organization</h2>
+        <Form>
+          <Form.Group>
+            <Form.Label>Organization name:</Form.Label>
+            <Form.Control type="String" placeholder="Enter organization name"/>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Phone</Form.Label>
+            <Form.Control type="number" placeholder="Enter phone number"/>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>E-mail</Form.Label>
+            <Form.Control type="String" placeholder="Enter organization mail"/>
+          </Form.Group>
+          <Button variant="primary" type="submit">Register</Button>
+        </Form>
+      </div>
+
+    )
+  }
+}
