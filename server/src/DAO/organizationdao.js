ba0cc1d4f3ec8b83e9ad61c50c0b663efb1dcdee
Pernille Kopperud <pernilko@stud.ntnu.no>
2020-01-08 11:53:04 +0100

    orgdao

---
+++ server/src/DAO/organizationdao.js
@@ -0,0 +1,45 @@
+//@flow
+const Dao = require("./dao.js");
+
+module.exports = class OrganizationDao extends Dao {
+    getAllOrganizations(callback: function) {
+        super.query(
+            "SELECT * FROM organization ", [], callback
+        );
+    }
+
+    getOrganization(org_id: number, callback: function) {
+        super.query(
+            "SELECT * FROM organization WHERE org_id = ?", [org_id],
+            callback
+        );
+    }
+
+    getOrgByEmail(email: string, callback: function) {
+        super.query(
+            "SELECT * FROM organization WHERE email = ?", [email],
+            callback
+        );
+    }
+
+    addOrganization(json: {org_name: string, phone: string, email: string}, callback: function) {
+        super.query(
+            "INSERT INTO organization(org_name, phone, email) VALUES (?,?,?)",
+            [json.org_name, json.phone, json.email], callback
+        );
+    }
+
+    deleteOrganization(org_id: number, callback: function){
+        super.query(
+            "DELETE FROM organization WHERE org_id = ?", [org_id],
+            callback
+        );
+    }
+
+    updateOrganization(json: {org_id: number, org_name: string, phone: string, email: string}, callback: function){
+        super.query(
+            "UPDATE organization SET org_name = ?, phone = ?, email = ? WHERE org_id = ?",
+            [json.org_name, json.phone, json.email, json.org_id], callback
+        );
+    }
+};
