0bf8a240fe78cb730d041f1ecfcbf3b56fadfa4c
tobiaand <tobiaand@stud.ntnu.no>
2020-01-08 16:01:01 +0100

    added some functions to userDao

--- server/src/DAO/userDao.js
+++ server/src/DAO/userDao.js
@@ -1,5 +1,7 @@
 //@flow
 const Dao = require("./dao.js");
+const bcrypt = require('bcryptjs');
+const saltRounds: number = 10;
 
 module.exports = class userDao extends Dao {
 
@@ -16,9 +18,35 @@ module.exports = class userDao extends Dao {
     setNormalPrivilegesId(id: number, callback: function){
         super.query("UPDATE user SET priviliges = 0 WHERE user_id = ?", [id], callback);
     }
-
     //method to set a user to default privileges in db, privileges = 0 => normal privilege
     setNormalPrivilegesEmail(email: string, org_id: number, callback: function){
         super.query("UPDATE user SET priviliges = 0 WHERE email = ? AND org_id = ?", [email, org_id], callback);
+    }
+
+    //retrieve the row of a user using the user_id
+    getUserById(id: number, callback: function){
+        super.query("SELECT * FROM user WHERE user_id = ?", [id], callback);
+    }
+
+    //retrieve the row of a user by using their email combined with their org_id
+    getUser(json: {email: string, org_id: number}, callback: function) {
+        super.query("SELECT email, org_id, password FROM user WHERE email=? AND org_id=?", [json.email, json.org_id], callback);
+    }
+
+    //add a new user to the DB by json
+    addUser(json: {email: string, org_id: number, privileges: number, user_name: string, password: string, address: string, phone: string, image: string}, callback: function) {
+        let salt: string = bcrypt.genSaltSync(saltRounds);
+        console.log(json.password);
+        let hash: string = bcrypt.hashSync(json.password, salt);
+        super.query(
+            "INSERT INTO user (org_id, email, privileges, user_name, password, address, phone, image) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+            [json.org_id, json.email, json.privileges, json.user_name, hash, json.address, json.phone, json.image],
+            callback
+        );
+    }
+
+    //delete a user using their id. Dont use without deleting rows in user_event first!
+    deleteUserById(id: number, callback: function){
+        super.query("DELETE FROM user WHERE user_id = ?", [id], callback);
     }
 };
