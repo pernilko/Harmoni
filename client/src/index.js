87c3e10393e1f6764640aab43297502b858db937
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2020-01-08 10:40:49 +0100

    user service class

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 import * as React from 'react';
 import { HashRouter, Route} from 'react-router-dom';
+import {Alert} from './widgets';
 
 const root = document.getElementById('root');
 if (root)
   ReactDOM.render(
     <HashRouter>
       <div>
+      <Alert/>
       </div>
     </HashRouter>,
     root
