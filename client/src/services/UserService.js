7a683db31010389e41e083adeaf1f3b6ac88d2c0
haavasma <haavasma@stud.ntnu.no>
2020-01-09 12:38:24 +0100

    added postToken in userService for

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -9,19 +9,19 @@ export class User {
     privileges: number;
     user_name: string;
     password: string;
-    adress: string;
+    address: string;
     phone: string;
     image: string;
     reg_date: string;
 
-    constructor(user_id: number, org_id: number, email: string, privileges: number, user_name: string, password: string, adress: string, phone: string, image: string, reg_date: string) {
+    constructor(user_id: number, org_id: number, email: string, privileges: number, user_name: string, password: string, address: string, phone: string, image: string, reg_date: string) {
         this.user_id = user_id;
         this.org_id = org_id;
         this.email = email;
         this.privileges = privileges;
         this.user_name = user_name;
         this.password = password;
-        this.adress = adress;
+        this.address = address;
         this.phone = phone;
         this.image = image;
         this.reg_date = reg_date;
@@ -37,6 +37,32 @@ class UserService {
             "password": password
         }).then(response=>response.data);
     }
+    register(org_id: number, email: string, privileges: number, user_name: string, password: string, address, phone: string, image: string){
+        return axios.post<{}, User>('http://localhost:8080/registrer',{
+            "org_id": org_id,
+            "email": email,
+            "privileges": privileges,
+            "user_name": user_name,
+            "password": password,
+            "address": address,
+            "phone": phone,
+            "image": image
+        }).then(response=>response.data);
+    }
+    postToken(email: string) {
+        return fetch("http://localhost:8080/token",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8",
+                    "x-access-token": localStorage.getItem("token")
+                },
+                body: JSON.stringify({"email": email})
+            })
+            .then(response => response.json());
+    }
+
+
 }
 
 export let userService = new UserService();
