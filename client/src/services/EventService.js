670ef00e29fc86b495d59894d0107c16ca143ace
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2020-01-09 10:06:42 +0100

    event service class

--- client/src/services/EventService.js
+++ client/src/services/EventService.js
@@ -1,6 +1,8 @@
 // @flow
 import axios from 'axios';
 
+const url = "http://localhost:8080/";
+
 export class Event {
     event_id: number;
     org_id: number;
@@ -22,3 +24,44 @@ export class Event {
         this.latitude = latitude;
     }
 }
+
+class EventService {
+    getAllEvents() {
+        return axios.get<Event[]>(url + "event/all").then(response => response.data);
+    }
+
+    getEventId(id: number) {
+        return axios.get<Event[]>(url + "event/" + id).then(response => response.data[0]);
+    }
+
+    postEvent(org_id: number, event_name: string, place: string, event_start: string, event_end: string, longitude: number, latitude: number) {
+        return axios.post<{}, Event>(url + "event/add", {
+            "org_id": org_id,
+            "event_name": event_name,
+            "place": place,
+            "event_start": event_start,
+            "event_end": event_end,
+            "longitude": longitude,
+            "latitude": latitude
+        }).then(response => response.data);
+    }
+
+    updateEvent(id: number, org_id: number, event_name: string, place: string, event_start: string, event_end: string, longitude: number, latitude: number) {
+        return axios.put<{}, Event>(url + "event/edit/"+id, {
+            "org_id": org_id,
+            "event_name": event_name,
+            "place": place,
+            "event_start": event_start,
+            "event_end": event_end,
+            "longitude": longitude,
+            "latitude": latitude
+        }).then(response => response.data);
+    }
+
+    deleteEvent(id: number) {
+        return axios.delete<Event, {}>(url + "event/delete/"+id).then(response => response.data);
+    }
+}
+
+export let eventService = new EventService();
+
