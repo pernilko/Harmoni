3be929a7f096ba3478e9a5c5023de2d4fa0ec689
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2020-01-09 11:31:00 +0100

    service

--- client/src/services/TicketService.js
+++ client/src/services/TicketService.js
@@ -1,7 +1,9 @@
 // @flow
 import axios from 'axios';
 
-export class TicketService {
+const url = "http://localhost:8080/";
+
+export class Ticket {
     ticket_id: number;
     event_id: number;
     ticket_type: string;
@@ -19,4 +21,53 @@ export class TicketService {
         this.price = price;
         this.amount_sold = amount_sold;
     }
+}
+
+class TicketService {
+    getAllTickets() {
+        return axios.get<Ticket[]>(url + "ticket/all").then(response => response.data);
+    }
+
+    getOneTicket(id: number) {
+        return axios.get<Ticket[]>(url + "ticket/"+id).then(response => response.data[0]);
+    }
+
+    getRemainingTickets(id: number) {
+        return axios.get<{}>(url + "ticket/remaining/"+id).then(response => response.data[0]);
+    }
+
+    addTicket(event_id: number, ticket_type: string, amount: number, description: string, price: number, amount_sold: number) {
+        return axios.post<{}, Ticket>(url + "ticket/add", {
+            "event_id": event_id,
+            "ticket_type": ticket_type,
+            "amount": amount,
+            "description": description,
+            "price": price,
+            "amount_sold": amount_sold
+        }).then(response => response.data);
+    }
+
+    addTicket(event_id: number, ticket_type: string, amount: number, description: string, price: number, amount_sold: number) {
+        return axios.post<{}, Ticket>(url + "ticket/add", {
+            "event_id": event_id,
+            "ticket_type": ticket_type,
+            "amount": amount,
+            "description": description,
+            "price": price,
+            "amount_sold": amount_sold
+        }).then(response => response.data);
+    }
+
+    updateTicket(id: number, event_id: number, ticket_type: string, amount: number, description: string, price: number, amount_sold: number) {
+        return axios.put<{}, Event>(url + "ticket/edit/"+id, {
+            "event_id": event_id,
+            "ticket_type": ticket_type,
+            "amount": amount,
+            "description": description,
+            "price": price,
+            "amount_sold": amount_sold
+        }).then(response => response.data);
+    }
+
+    
 }
