703c75272996e0562858d13e2d35e6b2609e619d
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2020-01-10 09:08:39 +0100

    id

--- server/src/DAO/eventDao.js
+++ server/src/DAO/eventDao.js
@@ -20,13 +20,6 @@ module.exports = class eventDao extends Dao{
         super.query("SELECT event_start, event_end FROM event WHERE event_id=?", [event_id], callback);
     }
 
-    addEvent(
-      json: {org_id: number, event_name: string, place: string, event_start: any, event_end: any, longitude: number, latitude: number} ,callback: function){
-        super.query( "INSERT INTO event (org_id, event_name, place, event_start, event_end, longitude, latitude) VALUES (?,?,?,?,?,?,?)",
-          [json.org_id, json.event_name, json.place, json.event_start, json.event_end, json.longitude, json.latitude],
-          callback);
-    }
-
     editEvent( event_id:number,
       json: {event_name: string, place: string, event_start: string, event_end: string, longitude: number, latitude: number}, callback:function){
         super.query("UPDATE event SET event_name=?, place=?, event_start=?, event_end=?, longitude=?, latitude=? WHERE event_id=?",
