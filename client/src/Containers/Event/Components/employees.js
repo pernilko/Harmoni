b56a299028870502eeb27aa6265f72da2d24344c
Maria Lande <mland@stud.ntnu.no>
2020-01-13 15:44:17 +0100

    hmmm

--- client/src/Containers/Event/Components/employees.js
+++ client/src/Containers/Event/Components/employees.js
@@ -7,64 +7,79 @@ import Accordion from "react-bootstrap/Accordion";
 import {Alert} from "../../../widgets";
 import {User, userService} from "../../../services/UserService";
 import {UserEvent} from "../../../services/UserEventService";
+import {Spinner} from "react-bootstrap";
+import {sharedComponentData} from "react-simplified";
 
 export class Employees extends Component <{employee: UserEvent}> {
     users: User[] = [];
     emp: UserEvent[] = [];
     position: string = "";
     render(){
-        return(
-            <Accordion>
-                <Card style={{border: "none"}}>
-                    <Card.Header style={{border: "none"}}>
-                        <Accordion.Toggle as={Button} variant="success" eventKey="0" style = {{float: "left"}}>
-                            Rediger
-                        </Accordion.Toggle>
-                        <button type="button" className="btn btn-danger" style={{marginLeft: 10+"px", float: "left"}} onClick={() => this.deleteEmployee(this.props.employee)}>Slett</button>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey="0" style={{border: "none"}}>
-                        <Card.Body>
-                            <form style={{padding: 20 + 'px', width: "100%", position: "sticky", overflow: "visible"}}>
-                                <div className="form-group">
-                                    <row>
-                                        <h4>Registrer ansatte for arrangement: </h4><br/>
-                                        <div className="form-group">
-                                            <label>Ansatt: </label>
-                                            <select className="form-control" id="userSelect">
-                                                {this.users.map(user => (
-                                                    <option value={user.user_id} selected={true}>{user.user_name}</option>
-                                                ))}
-                                            </select>
-                                        </div>
-                                        <div className="form-group">
-                                            <label>Stilling </label>
-                                            <input type="text" className="form-control" placeholder="stilling" value={this.position}
-                                                   onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.position = event.target.value)}/>
-                                        </div>
-                                        <br/>
-                                        <div className="form-group" align="center">
-                                            <Accordion.Toggle type="button"  as={Button} variant="success" eventKey="0" onClick={this.add}>
-                                                Lagre
-                                            </Accordion.Toggle>
-                                        </div>
-                                    </row>
-                                </div>
-                            </form>
-                        </Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-            </Accordion>
-        )
-    }
+            return (
+                <Accordion>
+                    <Card style={{border: "none"}}>
+                        <Card.Header style={{border: "none"}}>
+                            <Accordion.Toggle as={Button} variant="success" eventKey="0" style={{float: "left"}}>
+                                Rediger
+                            </Accordion.Toggle>
+                            <button type="button" className="btn btn-danger"
+                                    style={{marginLeft: 10 + "px", float: "left"}}
+                                    onClick={() => this.deleteEmployee(this.props.employee)}>Slett
+                            </button>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="0" style={{border: "none"}}>
+                            <Card.Body>
+                                <form style={{
+                                    padding: 20 + 'px',
+                                    width: "100%",
+                                    position: "sticky",
+                                    overflow: "visible"
+                                }}>
+                                    <div className="form-group">
+                                        <row>
+                                            <h4>Registrer ansatte for arrangement: </h4><br/>
+                                            <div className="form-group">
+                                                <label>Ansatt: </label>
+                                                <select multiple className="form-control" id="userSelect">
+                                                    {this.users.map(user => (
+                                                        <option value={user.user_id}
+                                                                selected={true}>{user.user_name}</option>
+                                                    ))}
+                                                </select>
+                                            </div>
+                                            <div className="form-group">
+                                                <label>Stilling </label>
+                                                <input type="text" className="form-control" placeholder="stilling"
+                                                       value={this.position}
+                                                       onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.position = event.target.value)}/>
+                                            </div>
+                                            <br/>
+                                            <div className="form-group" align="center">
+                                                <Accordion.Toggle type="button" as={Button} variant="success"
+                                                                  eventKey="0" onClick={this.add}>
+                                                    Lagre
+                                                </Accordion.Toggle>
+                                            </div>
+                                        </row>
+                                    </div>
+                                </form>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
+            )
+        }
+
     mounted() {
         let s: any = EmployeesDetails.instance();
         this.emp = s.emp;
-
         userService
-            .getUserByOrgId(13)
-            .then(res => {this.users = res;
-                console.log("hei"+this.users[0].user_name)})
+            .getUserByOrgId(userService.currentUser.org_id)
+            .then(res => {
+                this.users = res;
+            })
             .catch((error: Error) => Alert.danger("Noe gikk galt ved lasting av ansatte"));
+
     }
 
     add(){
