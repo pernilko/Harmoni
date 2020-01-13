9e976dcb0335355831567239455bab6850564ae7
tobiaand <tobiaand@stud.ntnu.no>
2020-01-13 09:12:38 +0100

    new map

--- client/src/Containers/Event/Components/map.js
+++ client/src/Containers/Event/Components/map.js
@@ -41,6 +41,7 @@ export default class MapContainer extends React.Component {
                     loadingElement={<div style={{ height: `100%` }} />}
                     containerElement={<div style={{ height: `400px` }} />}
                     mapElement={<div style={{ height: `100%` }} />}
+                    onClick={() => console.log("hei")}
                 />
             </div>
         )
