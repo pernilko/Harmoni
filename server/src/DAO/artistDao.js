8541756679bcd06280b567c56db115eb5cd7c61e
Pernille Kopperud <pernilko@stud.ntnu.no>
2020-01-14 14:33:58 +0100

    User event DAO and service

--- server/src/DAO/artistDao.js
+++ server/src/DAO/artistDao.js
@@ -21,11 +21,11 @@ module.exports = class artistDao extends Dao {
     }
 
     insertOne(json: {event_id: number, artist_name: string, riders: Blob, hospitality_riders: Blob,
-                  artist_contract: Blob, email: string, phone: string, image: Blob}, callback: function) {
+                  artist_contract: Blob, email: string, phone: string}, callback: function) {
         console.log(json.riders.toString());
         super.query(
-            "INSERT INTO artist (event_id, artist_name, riders, hospitality_riders, artist_contract, email, phone, image) values (?,?,?,?,?,?,?,?)",
-            [json.event_id, json.artist_name, json.riders, json.hospitality_riders, json.artist_contract, json.email, json.phone, json.image],
+            "INSERT INTO artist (event_id, artist_name, riders, hospitality_riders, artist_contract, email, phone) values (?,?,?,?,?,?,?,?)",
+            [json.event_id, json.artist_name, json.riders, json.hospitality_riders, json.artist_contract, json.email, json.phone],
             callback
         );
     }
