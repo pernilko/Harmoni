2733b4560d70503649b8d2f196b6e77d9a66911f
Julie <julie2109@hotmail.com>
2020-01-17 13:58:31 +0100

    pending

--- server/src/DAO/eventDao.js
+++ server/src/DAO/eventDao.js
@@ -45,8 +45,7 @@ module.exports = class eventDao extends Dao{
     }
 
 
-editEvent(event_id: number, json: {event_name: string, place: string, description: string, event_start: Date, event_end: Date, longitude: number, latitude: number, image: number}, callback:function) {
-
+    editEvent(event_id: number, json: {event_name: string, place: string, description: string, event_start: Date, event_end: Date, longitude: number, latitude: number, image: number}, callback:function) {
       super.query("UPDATE event SET event_name=?, place=?, description=?, event_start=?, event_end=?, longitude=?, latitude=?, image=? WHERE event_id=?",
                   [json.event_name, json.place, json.description, json.event_start, json.event_end, json.longitude, json.latitude, json.image, event_id],
                   callback);
