37dfff408849db929efe54fac8033f6711409195
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2020-01-18 11:58:57 +0100

    hallo

--- client/src/Containers/Navbar/Components/search.js
+++ client/src/Containers/Navbar/Components/search.js
@@ -5,7 +5,6 @@ import {Container, Image, ListGroup, ListGroupItem, Nav} from "react-bootstrap";
 import {sharedComponentData} from "react-simplified";
 import {userService} from "../../../services/UserService";
 
-
 export class SearchResults extends Component <{match: {params: {search: string}}}> {
     events: Event[] | any = [];
     org_id: number = 0;
@@ -16,81 +15,76 @@ export class SearchResults extends Component <{match: {params: {search: string}}
     event_end: string = "";
 
     render() {
-        if (userService.currentUser) {
-            if (!this.loaded) {
-                this.org_id = userService.currentUser.org_id;
-                this.load(this.org_id);
-                this.loaded = true;
-            }
-            return (
-                <div className="searchResults">
-                    <Container style={{padding: 0}}>
-                        <div className="card-header" style={{color: 'white', backgroundColor: '#53265F'}}>
-                            <h4> Søkeresultater for: {this.props.match.params.search}</h4></div>
-                        <a href={"#/search_result/" + this.props.match.params.search} onClick={this.upcoming}>Kommende </a>
-                        <a href={"#/search_result/" + this.props.match.params.search} onClick={this.finished}>Utløpte </a>
-                        <a href={"#/search_result/" + this.props.match.params.search} onClick={this.show}> Dato
-                            <div hidden={this.hidden}>
-                                <div className="col">
-                                    <label>Søk fra dato:</label>
-                                    <input id="help" className="form-control" type="date" value={this.event_start}
-                                           onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.event_start = event.target.value)}/>
-                                </div>
-                                <div className="col">
-                                    <label>til dato:</label>
-                                    <input id="help" className="form-control" type="date" value={this.event_end}
-                                           onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.event_end = event.target.value)}/>
-                                </div>
-                                <button className="btn btn-primary submit" style={{margin:10 +'px'}} onClick={this.date}>Søk</button>
+        return (
+            <div className="searchResults">
+                <Container style={{padding: 0}}>
+                    <div className="card-header" style={{color: 'white', backgroundColor: '#53265F'}}>
+                        <h4> Søkeresultater for: {this.props.match.params.search}</h4></div>
+                    <a href={"#/search_result/" + this.props.match.params.search} onClick={this.upcoming}>Kommende </a>
+                    <a href={"#/search_result/" + this.props.match.params.search} onClick={this.finished}>Utløpte </a>
+                    <a href={"#/search_result/" + this.props.match.params.search} onClick={this.show}> Dato
+                        <div hidden={this.hidden}>
+                            <div className="col">
+                                <label>Søk fra dato:</label>
+                                <input id="help" className="form-control" type="date" value={this.event_start}
+                                        onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.event_start = event.target.value)}/>
                             </div>
-                        </a>
+                            <div className="col">
+                                <label>til dato:</label>
+                                <input id="help" className="form-control" type="date" value={this.event_end}
+                                        onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.event_end = event.target.value)}/>
+                            </div>
+                            <button className="btn btn-primary submit" style={{margin:10 +'px'}} onClick={this.date}>Søk</button>
+                        </div>
+                    </a>
 
-                        {this.events.map(e => (
-                            <div style={{maxHeight: 100 + '%'}}>
-                                <ListGroup>
-                                    <ListGroupItem style={{width: 100 + '%', height: "200px", padding: 0}}>
-                                        <div className="row" style={{paddingLeft: 15 + "px", paddingRight: 15 + "px"}}>
-                                            <div className="col-5" style={{padding: 0}}>
-                                                <Image
-                                                    src="https://kampanje.com/contentassets/0c30c67529294a8c8e59d84740c27e90/eventbilde-sponsor-og-event.jpg?width=1600&height=1300&mode=carve"
-                                                    height="200px" width="300px" style={{objectFit: "cover"}}/>
-                                            </div>
-                                            <div className="col-7" style={{padding: 0}}>
-                                                <p style={{
-                                                    color: "#D35400",
-                                                    fontSize: '25px',
-                                                    fontWeight: "bold"
-                                                }}>Arrangement
-                                                    start: {e.event_start.slice(8, 10) + "." + e.event_start.slice(5, 7) + "." + e.event_start.slice(0, 4)
-                                                    + " kl: " + e.event_start.slice(11, 16)}</p>
-                                                <Nav.Link href={"#/showEvent/" + e.event_id} style={{padding: 0}}><p
-                                                    style={{
-                                                        color: 'black',
-                                                        fontSize: '40px',
-                                                        fontWeight: "bold"
-                                                    }}>{e.event_name}</p></Nav.Link>
-                                                <p style={{color: 'black', fontSize: '20px'}}>{(e.place)}</p>
-                                            </div>
+                    {this.events.map(e => (
+                        <div style={{maxHeight: 100 + '%'}}>
+                            <ListGroup>
+                                <ListGroupItem style={{width: 100 + '%', height: "200px", padding: 0}}>
+                                    <div className="row" style={{paddingLeft: 15 + "px", paddingRight: 15 + "px"}}>
+                                        <div className="col-5" style={{padding: 0}}>
+                                            <Image
+                                                src="https://kampanje.com/contentassets/0c30c67529294a8c8e59d84740c27e90/eventbilde-sponsor-og-event.jpg?width=1600&height=1300&mode=carve"
+                                                height="200px" width="300px" style={{objectFit: "cover"}}/>
                                         </div>
-                                    </ListGroupItem>
-                                </ListGroup>
-                            </div>
-                        ))}
-                    </Container>
-                </div>
-            )
-        }
-        else {
-            return <div>Something went wrong :(</div>
-        }
+                                        <div className="col-7" style={{padding: 0}}>
+                                            <p style={{
+                                                color: "#D35400",
+                                                fontSize: '25px',
+                                                fontWeight: "bold"
+                                            }}>Arrangement
+                                                start: {e.event_start.slice(8, 10) + "." + e.event_start.slice(5, 7) + "." + e.event_start.slice(0, 4)
+                                                + " kl: " + e.event_start.slice(11, 16)}</p>
+                                            <Nav.Link href={"#/showEvent/" + e.event_id} style={{padding: 0}}><p
+                                                style={{
+                                                    color: 'black',
+                                                    fontSize: '40px',
+                                                    fontWeight: "bold"
+                                                }}>{e.event_name}</p></Nav.Link>
+                                            <p style={{color: 'black', fontSize: '20px'}}>{(e.place)}</p>
+                                        </div>
+                                    </div>
+                                </ListGroupItem>
+                            </ListGroup>
+                        </div>
+                    ))}
+                </Container>
+            </div>
+        )
     }
 
+    mounted() {
+        console.log("HEI");
+        this.load();
+    }
 
-    load(id: number) {
-        eventService.getEventbySearch(this.props.match.params.search, id)
+    load() {
+    this.org_id = userService.currentUser.org_id;
+    console.log("ORG_ID: ", this.org_id);
+    eventService.getEventbySearch(this.props.match.params.search, this.org_id)
             .then(event => this.events = event)
-            .catch((error: Error) => console.log(error.message));
-        this.loaded = true;
+            .catch((error: Error) => console.log(error.message))
     }
 
     show(){
@@ -103,13 +97,10 @@ export class SearchResults extends Component <{match: {params: {search: string}}
 
 
     finished(){
-
         this.events = this.events.filter(a => new Date(a.event_start.slice(0,10)) - new Date < 0)
-
-
     }
 
-    date(){
+    date() {
         this.events = this.events.filter(a => new Date(a.event_start.slice(0,10)) >= this.event_start && a.event_end.slice(0,10) <= this.event_end);
         this.hidden = true;
     }
