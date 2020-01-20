446985f83bbd210caa61c53f306cc243aff78279
Brigittb <brigittb@stud.ntnu.no>
2020-01-20 23:19:05 +0100

    login

--- client/src/Containers/Login/Components/newLogin.js
+++ client/src/Containers/Login/Components/newLogin.js
@@ -1,8 +1,9 @@
-import { User } from '../../../services/UserService';
+import { User, userService } from '../../../services/UserService';
 import { Organization } from '../../../services/OrganizationService';
 import "./login.css";
 import {Component} from 'react-simplified';
 import * as React from 'react';
+import { Alert } from '../../../widgets';
 
 export class NewLogin extends Component {
   organization: Organization = new Organization();
@@ -115,11 +116,46 @@ export class NewLogin extends Component {
 
   }
 
-  register(){
+  next() {
+    //Reveal a new component for registering an Admin-user
+    console.log(this.organization.org_name);
+    console.log(this.organization.phone);
+    console.log(this.organization.email);
+    if (this.organization.org_name.length !== 0 &&
+      this.organization.email.length !== 0 &&
+      this.organization.phone !== 0 &&
+      emailRegEx.test(this.organization.email)) {
+      this.hidden = false;
 
+    } else {
+      Alert.danger("Alle felt må fylles ut, og gyldig e-post addresse må skrives inn");
+    }
   }
 
-
+  register() {
+    // Register
+    if (this.repeatedPassword != this.user.password || this.user.password.length < 8) {
+      Alert.danger("Passord må være like og ha minst 8 tegn");
+    } else if (!emailRegEx.test(this.user.email)) {
+      Alert.danger("Ikke gyldig E-mail addresse");
+    } else if (this.user.email.length !== 0 && this.user.address.length !== 0 && this.user.user_name.length !== 0
+      && this.user.phone.length !== 0 && emailRegEx.test(this.user.email)) {
+      console.log(this.organization.org_name);
+      userService.verifiserAdminOgOrg(this.organization.org_name,
+        this.organization.email,
+        this.organization.phone,
+        this.user.email,
+        1,
+        this.user.user_name,
+        this.user.password, this.user.address, this.user.phone).then(res => {
+        Alert.success("En verifiserings-Email har blitt sendt til din personlige Email-bruker");
+      }).catch((error: Error) => {
+        Alert.danger(error.message);
+      })
+    } else {
+      Alert.danger("alle felt må fylles og passord må ha minst 8 bokstaver");
+    }
+  }
 }
 
 
