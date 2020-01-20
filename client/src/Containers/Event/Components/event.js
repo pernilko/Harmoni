eb820e227cd64707571bf94bdcf1fc957e111582
Pernille Kopperud <pernilko@stud.ntnu.no>
2020-01-20 15:24:28 +0100

    fixes

--- client/src/Containers/Event/Components/event.js
+++ client/src/Containers/Event/Components/event.js
@@ -32,49 +32,100 @@ export class EventDetails extends Component<{ match: { params: { id: number } } 
     render() {
         if(this.loaded){
             let e: Event = this.state["event"];
-            return (
-                <div className={"w-50 mx-auto shadow-lg mt-4"}>
-                    <div className="card card-cascade wider reverse C">
-                        <div className="view view-cascade overlay">
-                            <img className="card-img-top shadow-lg"
-                                 src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
-                                 alt="Card image cap"></img>
-                            <a href="#!">
-                                <div className="mask rgba-white-slight"></div>
-                            </a>
-                        </div>
-                        <div className="card-body card-body-cascade text-center">
-                            <h4 className="card-title"><strong>{e.event_name}</strong></h4>
-                            <h6 className="font-weight-bold indigo-text py-2">{e.place}</h6>
-                            <h6 className="card-subtitle mb-2 text-muted"> <b></b> {e.event_start.slice(0, 10)}, {e.event_start.slice(11, 16)}-{e.event_end.slice(11, 16)}</h6>
-                            <p className="card-text">{e.description}</p>
-                            <p>HEI - INFO OM ARRANGEMENT</p>
-                            <a href="#" className="card-link">Aksepter</a>
-                            <a href="#" className="card-link">Avslå</a>
-                            <a href={"#/editEvent/"+this.event_id} className="card-link">Rediger</a>
-                            <Popup trigger = {<a className="card-link">Avlys arrangement</a>} >
-                                { close => (
-                                  <div>
-                                      <p><b>Vil du avlyse dette arrangementet?</b></p>
-                                      <button className="btn btn-warning float-left ml-3" onClick={() => {close();}}> Nei</button>
-                                      <button className="btn btn-success float-right mr-3" onClick={() => this.cancelled(this.event_id)}>Ja</button>
-                                  </div>
-                                )}
-                            </Popup>
-                            <a href={"#/showEvent/" + this.event_id} className="card-link" onClick={this.show}> Rapporter problem
-                                <div hidden={this.hidden}>
-                                    <textarea rows="4" cols="40" style={{margin: '10px',}} placeholder="Beskriv feilmelding"
-                                              onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.bugreport = event.target.value)}/>
-                                    <br/>
-                                    <button className="btn btn-primary submit" style={{margin:10 +'px'}} onClick={this.sendReport}>Rapporter problem</button>
-                                </div>
-                            </a>
-                            <br/>
-                            <MapContainer lat={e.latitude} lng={e.longitude} show={true}/>
+            if(e.completed !== -1) {
+                return (
+                    <div className={"w-50 mx-auto shadow-lg mt-4"}>
+                        <div className="card card-cascade wider reverse C">
+                            <div className="view view-cascade overlay">
+                                <img className="card-img-top shadow-lg"
+                                     src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
+                                     alt="Card image cap"></img>
+                                <a href="#!">
+                                    <div className="mask rgba-white-slight"></div>
+                                </a>
+                            </div>
+                            <div className="card-body card-body-cascade text-center">
+                                <h4 className="card-title"><strong>{e.event_name}</strong></h4>
+                                <h6 className="font-weight-bold indigo-text py-2">{e.place}</h6>
+                                <h6 className="card-subtitle mb-2 text-muted">
+                                    <b></b> {e.event_start.slice(0, 10)}, {e.event_start.slice(11, 16)}-{e.event_end.slice(11, 16)}
+                                </h6>
+                                <p className="card-text">{e.description}</p>
+                                <p>HEI - INFO OM ARRANGEMENT</p>
+                                <a href="#" className="card-link">Aksepter</a>
+                                <a href="#" className="card-link">Avslå</a>
+                                <a href={"#/editEvent/" + this.event_id} className="card-link">Rediger</a>
+                                <Popup trigger={<a className="card-link">Avlys arrangement</a>}>
+                                    {close => (
+                                        <div>
+                                            <p><b>Vil du avlyse dette arrangementet?</b></p>
+                                            <button className="btn btn-warning float-left ml-3" onClick={() => {
+                                                close();
+                                            }}> Nei
+                                            </button>
+                                            <button className="btn btn-success float-right mr-3"
+                                                    onClick={() => this.cancelled(this.event_id)}>Ja
+                                            </button>
+                                        </div>
+                                    )}
+                                </Popup>
+                                <a href={"#/showEvent/" + this.event_id} className="card-link"
+                                   onClick={this.show}> Rapporter problem
+                                    <div hidden={this.hidden}>
+                                        <textarea rows="4" cols="40" style={{margin: '10px',}}
+                                                  placeholder="Beskriv feilmelding"
+                                                  onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.bugreport = event.target.value)}/>
+                                        <br/>
+                                        <button className="btn btn-primary submit" style={{margin: 10 + 'px'}}
+                                                onClick={this.sendReport}>Rapporter problem
+                                        </button>
+                                    </div>
+                                </a>
+                                <br/>
+                                <MapContainer lat={e.latitude} lng={e.longitude} show={true}/>
+                            </div>
                         </div>
                     </div>
-                </div>
-            );
+                );
+            }else{
+                return (
+                    <div className={"w-50 mx-auto shadow-lg mt-4"}>
+                        <div className="card card-cascade wider reverse C">
+                            <div className="view view-cascade overlay container">
+                                <img className="card-img-top shadow-lg"
+                                     src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
+                                     alt="Card image cap"
+                                     style={{filter:"grayscale(90%"}}/>
+                                <div className="text-block">
+                                    <p>Avlyst</p>
+                                </div>
+                                <a href="#!">
+                                    <div className="mask rgba-white-slight"> </div>
+                                </a>
+                            </div>
+                            <div className="card-body card-body-cascade text-center">
+                                <h4 className="card-title"><strong>{e.event_name}</strong></h4>
+                                <h6 className="font-weight-bold indigo-text py-2">{e.place}</h6>
+                                <h6 className="card-subtitle mb-2 text-muted"> <b></b> {e.event_start.slice(0, 10)}, {e.event_start.slice(11, 16)}-{e.event_end.slice(11, 16)}</h6>
+                                <p className="card-text">{e.description}</p>
+                                <p className="text-muted">INFO ARRANGEMENT KAN VÆRE HER </p>
+                                <a href={"#/showEvent/" + this.event_id} className="card-link" onClick={this.show}> Rapporter problem
+                                    <div hidden={this.hidden}>
+                  <textarea rows="4" cols="40"
+                            style={{margin: '10px'}}
+                            placeholder="Beskriv feilmelding"
+                            onChange={(event: SyntheticInputEvent<HTMLInputElement>) => (this.bugreport = event.target.value)}/>
+                                        <br/>
+                                        <button className="btn btn-primary submit" style={{margin:10 +'px'}} onClick={this.sendReport}>Rapporter problem</button>
+                                    </div>
+                                </a>
+                                <br/>
+                                <MapContainer lat={e.latitude} lng={e.longitude} show={true}/>
+                            </div>
+                        </div>
+                    </div>
+                );
+            }
         }else{
             return <div/>
         }
