8fab6df3b08e27b5969319aa72f7315f6d5c50bd
Pernille Kopperud <pernilko@stud.ntnu.no>
2020-01-21 09:30:35 +0100

    testing

--- server/tests/eventdaoTest.js
+++ server/tests/eventdaoTest.js
@@ -37,7 +37,7 @@ test("Retrieve all events", done =>{
   }
 
   eventDao.getAll(callback);
-});
+}, 30000);
 
 
 test("Get an event", done =>{
@@ -52,7 +52,7 @@ test("Get an event", done =>{
   }
 
   eventDao.getEvent(1, callback);
-});
+}, 30000);
 
 
 test("Get events for one organization", done => {
@@ -61,12 +61,12 @@ test("Get events for one organization", done => {
       "Test callback: status =" + status + ", data =" + data + JSON.stringify(data)
     );
 
-    expect(data.length).toBe(2)
+    expect(data.length).toBe(2);
     done();
   }
 
   eventDao.getEventOrg(2, callback);
-});
+}, 30000);
 
 test("Get events for one user", done =>{
   function callback (status, data) {
@@ -79,7 +79,7 @@ test("Get events for one user", done =>{
   }
 
   eventDao.getEventUser(1, callback);
-});
+}, 30000);
 
 test("Get event location", done =>{
   function callback (status, data) {
@@ -93,7 +93,7 @@ test("Get event location", done =>{
   }
 
   eventDao.getEventLocation(1, callback);
-});
+}, 30000);
 
 test("Edit an event", done => {
   function callback(status, data) {
@@ -105,7 +105,7 @@ test("Edit an event", done => {
   }
 
   eventDao.editEvent(1, {event_name: "Cool event", place: "Mysen", description: "Gutta", event_start: "2020-01-26", event_end: "2020-01-26", longitude: 1, latitude: 2, image: ""}, callback)
-});
+}, 30000);
 
 test("Test edit", done =>{
   function callback (status, data) {
@@ -119,7 +119,7 @@ test("Test edit", done =>{
   }
 
   eventDao.getEvent(1, callback);
-});
+}, 30000);
 
 test("Test employees for an event", done =>{
   function callback (status, data) {
@@ -132,7 +132,7 @@ test("Test employees for an event", done =>{
   }
 
   eventDao.getUsersForEvent(1, callback);
-});
+}, 30000);
 
 test("Test user accepting an event", done =>{
   function callback (status, data) {
@@ -145,7 +145,7 @@ test("Test user accepting an event", done =>{
   }
 
   eventDao.setAccepted({user_id: 1, event_id: 1, accepted: 2}, callback);
-});
+}, 30000);
 
 test("Test event search", done =>{
   function callback (status, data) {
@@ -159,4 +159,4 @@ test("Test event search", done =>{
   }
 
   eventDao.getEventbySearch("Fotball", 3, callback);
-});
+}, 30000);
