42c3410401ef879dc67a2f52f7265ca25350a35d
tobiaand <tobiaand@stud.ntnu.no>
2020-01-23 13:18:32 +0100

    added jsdoc for UserService

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -6,7 +6,9 @@ import {Organization, organizationService} from "./OrganizationService";
 import {Artist, File} from "./ArtistService";
 
 let url: string = "http://localhost:8080/";
-
+/**
+    User - Klasse som beskriver en rad i Usertabellen.
+ */
 export class User {
     user_id: number = -1;
     org_id: number = -1;
@@ -37,10 +39,17 @@ export class User {
     }
      */
 }
+
+/**
+    UserService - Klassen brukes for å håndtere informasjon tilknytta en bruker.
+ */
 class UserService {
     currentUser: User;
-    //auto login
 
+    /**
+        autoLogin - Sjekke om det ligger en gyldig token i localstorage. Om det er tilfellet så refresher den tokenen slik at brukeren er logget inn i en time til.
+        @return et JSON-objekt med variablene token, og user_id. Token er tokenen som verifiserer at brukeren er logget inn user_id er id til brukeren som er logga inn.
+     */
     autoLogin(){
         console.log("auto-logging in with token from localStorage: " + localStorage.getItem("token"));
         if (localStorage.getItem("token")) {
@@ -53,6 +62,7 @@ class UserService {
                 }
             })
                 .then(response => {
+                    //response er token og id til brukeren
                     if (response.data.jwt) {
                         localStorage.setItem("token", response.data.jwt);
                         console.log("user_id: " + response.data.user_id);
@@ -70,7 +80,13 @@ class UserService {
         }
     }
 
-    //for logging in
+    /**
+        logIn - funksjon for å logge inn på en bruker, tar inn informasjon fra brukeren og returner en token om informasjonen var gyldig.
+        @parameter {number} org_id - id til organisasjonen som brukeren skal logge inn hos.
+        @parameter {string} email - emailen til brukeren som forsøker å logge inn.
+        @parameter {string} password - passordet som brukeren forsøker å logge inn med.
+        @return et JSON-objekt med variablene token, og user_id. Token er tokenen som verifiserer at brukeren er logget inn user_id er id til brukeren som er logga inn.
+     */
     logIn(org_id: number, email: string, password: string){
         return axios.post<{}, {jwt: string}>(url+'login', {
             "org_id":org_id,
@@ -87,7 +103,20 @@ class UserService {
             }
         });
     }
+
     //for registering a new user
+    /**
+        register - Legger til en ny bruker i DB.
+        @parameter {number} org_id - id til organisasjonen som brukeren skal logge inn hos.
+        @parameter {string} email - emailen til brukeren som forsøker å logge inn.
+        @parameter {number} privileges - et tall som beskriver om brukeren er en vanlig bruker (0), eller om brukeren er en admin (alle andre tall enn 0).
+        @parameter {string} user_name - navnet til brukeren som skal registrere seg.
+        @parameter {string} password - passordet som brukeren forsøker å logge inn med.
+        @parameter {string} address - addressen til brukeren som skal registrere seg.
+        @parameter {string} phone - mobilnummer til brukeren som skal registrere seg.
+        @parameter {string} image - google cloud URL til profilbildet til brukeren som skal registrere seg.
+        @return antallet rader som ble påvirket av kallet.
+     */
     register(org_id: number, email: string, privileges: number, user_name: string, password: string, address: string, phone: string, image: string){
         return axios.post<{}, User>(url+'register',{
             "org_id": org_id,
@@ -101,7 +130,12 @@ class UserService {
         }).then(response=>response.data);
     }
 
-
+    /**
+        addProfilePicture - oppdaterer en bruker sitt profilbilde
+        @parameter {number} user_id - Id til brukeren som skal endre bildet sitt.
+        @parameter {File} picture - bildet som skal lastet opp til google cloud.
+        @return antallet rader som ble påvirket av kallet
+     */
     addProfilePicture(user_id: number, picture: File) {
         let fd:FormData = new FormData();
         fd.append("myFile", picture);
@@ -115,14 +149,35 @@ class UserService {
             });
     }
 
+    /**
+        updateEmail - Metode for å endre emailen til en bruker
+        @parameter {number} user_id - id til brukeren som skal endre sin email.
+        @parameter {string} email - den nye emailen til brukeren.
+        @return Antallet rader som ble påvirket av kallet.
+     */
     updateEmail(user_id: number, email: string){
         return axios.put<{}, User>(url + 'Profile/editEmail/' + user_id,
           {"email": email}) .then(response => response.data);
     }
+
+    /**
+        updateEmail - Metode for å endre profilbildet til en bruker
+        @parameter {number} user_id - id til brukeren som skal endre sin email.
+        @parameter {string} image - det nye bildet til brukeren.
+        @return Antallet rader som ble påvirket av kallet.
+     */
     updateImage(user_id:number, image:string){
         return axios.put<{}, User>(url + 'Profile/editImage/' + user_id,
           {"image": image}) .then(response => response.data);
     }
+
+    /**
+        updateEmail - Metode for å endre adresse og/eller mobilnr til en bruker
+        @parameter {number} user_id - id til brukeren som skal endre sin email.
+        @parameter {string} adresse - den nye adressen til brukeren.
+        @parameter {string} phone - mobilnr til brukeren
+        @return Antallet rader som ble påvirket av kallet.
+     */
     updateInfo(user_id:number, address:string, phone: string){
         return axios.put<{}, User>(url + 'Profile/editInfo/' + user_id, {
             "address": address,
@@ -130,14 +185,30 @@ class UserService {
         }) .then(response => response.data);
     }
 
+    /**
+        updateEmail - Metode for å endre rettighetene til en bruker til en bruker
+        @parameter {number} p_create_event - tallverdi som er større enn 0 om brukeren har lov til å opprette arrangement for en organisasjon.
+        @parameter {number} p_read_contract - tallverdi som er større enn 0 om brukeren har lov til å lese kontrakter for organisasjonen.
+        @parameter {number} p_read_riders - tallverdi som er større enn 0 om brukeren har lov til å lese rider-avtalene.
+        @parameter {number} p_archive - tallverdi som er større enn 0 om brukeren har lov til å arkivere arrangement.
+        @return Antallet rader som ble påvirket av kallet.
+     */
     updatePrivileges(user_id: number, p_create_event: number, p_read_contract: number, p_read_riders: number, p_archive: number){
         return axios.put<{}, User>(url+ 'user/updatePrivileges/'+ user_id, {
             "p_create_event": p_create_event,
             "p_read_contract": p_read_contract,
             "p_read_riders": p_read_riders,
-            "p_archive":p_archive
+            "p_archive": p_archive
         }).then(response=>response.data);
     }
+
+    /**
+        updateEmail - Metode for å endre brukernavn/passord til en bruker
+        @parameter {number} user_id - id til brukeren som skal endre sin email.
+        @parameter {string} user_name - det nye navnet til brukeren.
+        @parameter {string} password - det nye passordet til brukeren.
+        @return Antallet rader som ble påvirket av kallet.
+     */
     updateUsernamePassword(user_id:number, user_name: string, password: string){
         return axios.put<{}, User>(url + 'Profile/edit/' + user_id, {
             "user_name": user_name,
@@ -145,40 +216,53 @@ class UserService {
         }).then(response => response.data);
     }
 
+    /**
+        updateEmail - Metode for å endre navnet til en bruker til en bruker
+        @parameter {number} user_id - id til brukeren som skal endre sin email.
+        @parameter {string} user_name - det nye brukernavnet til en bruker.
+        @return Antallet rader som ble påvirket av kallet.
+     */
     updateUserName(user_id: number, user_name: string){
         return axios.put<{}, User>(url + 'Profile/updateUsername/'+ user_id, {
             "user_name": user_name
         }).then(response=>response.data);
     }
 
+    /**
+        deleteUser - metode for å slette en bruker.
+        @parameter {number} user_id - id til brukeren som skal slettes.
+        @return antallet rader som ble påvirket av kallet.
+     */
     deleteUser(user_id: number){
         return axios.delete<{}, User>(url + 'user/delete/' + user_id).then(response => response.data);
     }
 
-
+    /**
+        getUser - henter en bruker med en gitt id.
+        @parameter {number} user_id - id til brukeren som skal hentes.
+        @return User[] raden i Usertabellen til brukeren som ble hentet
+     */
     getUser(user_id){
         return axios.get<User>(url+ 'user/'+ user_id).then(response=>response.data[0]);
     }
-    //to refresh token
-    //not tested
-    postToken(user_id: string) {
-        return fetch(url + 'token',
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json; charset=utf-8",
-                    "x-access-token": localStorage.getItem("token")
-                },
-                body: JSON.stringify({"user_id": user_id})
-            })
-            .then(response => response.json());
-    }
 
+    /**
+        getuserByOrgId - henter alle brukere som er tilknytta en organisasjon.
+        @parameter {number} org_id - id til organisasjonen metoden skal hente alle brukerne til.
+        @return User[] alle brukerne tilknytta organisasjonen
+     */
     getUserByOrgId(org_id: number){
         console.log("ORG_ID: ", org_id)
         return axios.get<User[]>(url +"user/all/"+ org_id).then(response => response.data);
     }
 
+    /**
+        resetPass - oppdatere passordet til en bruker
+        @parameter {number} org_id - id til organisasjonen som brukeren tilhører.
+        @parameter {string} email - eposten til brukeren som skal endre sitt passord.
+        @parameter {string} password - det nye passordet til brukeren.
+        @return antallet rader som ble påvirket av kallet
+     */
     resetPass(org_id: number, email: string, password: string) {
         return axios.put<{}, User>(url + "user/resetPass", {
             "org_id": org_id,
@@ -187,10 +271,28 @@ class UserService {
         }).then(res => res.data);
     }
 
+    /**
+        getAdminByOrgId - Henter adminbrukerne til en gitt organisasjon
+        @parameter {number} org_id - id til organisasjonen det er snakk om.
+        @return User[], liste med Userobjekt som er administratorer i organisasjonen.
+     */
     getAdminByOrgId(org_id: number){
         return axios.get<User[]>(url +"user/admin/"+ org_id).then(response => response.data)
     }
     
+    /**
+        verifiserAdminOgOrg - sender en email til ny admin for å bekrefte den når de har laget en organisasjon. Det sendes med mye informasjon om administratorbrukeren fordi den ikke opprettes før de trykker på lenken, så informasjonen som lager bruker blir kryptert med i lenken.
+        @parameter {string} org_name - navnet på organisasjonen.
+        @parameter {string} org_email - emailen til organisasjonen selv.
+        @parameter {string} org_phone - mobilnummeret til organisasjonen selv.
+        @parameter {string} user_email - epostadressen til adminbrukeren som skal motta eposten.
+        @parameter {number} user_privileges - et nummer som er større enn 0 dersom brukeren er av typen administrator.
+        @parameter {string} user_user_name - navnet til administratorbrukeren.
+        @parameter {stirng} user_password - passordet til administratorbrukeren.
+        @parameter {string} user_adress - adressen til den nye adminstratorbrukeren.
+        @parameter {string} user_phone - mobilnummeret til den nye administratorbrukeren
+        @return antallet rader som ble påvirket.
+     */
     verifiserAdminOgOrg(org_name: string, org_email: string, org_phone: string, user_email: string, user_privileges, user_user_name, user_password, user_address, user_phone){
         return axios.post<{}>(url+"verifyEmail", {
             "org_name": org_name,
@@ -205,6 +307,11 @@ class UserService {
         }).then(res=>res.data);
     }
 
+    /**
+        makeAdmin - endrer rettighetene til en bruker 
+        @parameter {number} user_id - id til brukeren som skal bli en administrator.
+        @return antallet rader som ble påvirket.
+     */
     makeAdmin(user_id: number) {
         return axios.put<{}>(url + "user/makeAdmin/"+user_id).then(res => res.data);
     }
