872b7ba9be523a6102e3cecce699704bcf3457d6
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2020-01-24 20:37:35 +0100

    h

--- server/src/DAO/userEventDao.js
+++ server/src/DAO/userEventDao.js
@@ -1,4 +1,3 @@
-//@flow
 const Dao = require("./dao.js");
 
 /**
@@ -14,7 +13,7 @@ module.exports = class userEventDao extends Dao{
         @parameter {number} accepted - et tall som beskriver om brukeren har godtatt (1), avslått (0), eller ikke svart på om de kan ta vakta (alt annet).
         @parameter {function} callback - funksjon som returner data fra kallet
      */
-    addUserEvent(json: {user_id: number, event_id: number, job_position: string, accepted: number}, callback: function){
+    addUserEvent(json, callback){
         super.query("INSERT INTO user_event(user_id, event_id, job_position, accepted) VALUES (?,?,?,?)",
             [json.user_id, json.event_id, json.job_position, json.accepted], callback);
     }
@@ -24,7 +23,7 @@ module.exports = class userEventDao extends Dao{
         @parameter {number} event_id - id til arrangementet som man henter vaktene til.
         @parameter {function} callback - funksjon som returner data fra kallet
      */
-    getAllbyId(event_id: number, callback: function){
+    getAllbyId(event_id, callback){
         super.query(
             "SELECT user_event.user_id, event_id, job_position, accepted, user_name, email FROM user_event JOIN user ON (user_event.user_id = user.user_id) WHERE event_id = ?",
             [event_id],  callback
@@ -37,7 +36,7 @@ module.exports = class userEventDao extends Dao{
         @parameter {number} event_id - id til eventet som har et vakt som skal slettes.
         @parameter {function} callback - funksjon som returner data fra kallet
      */
-    deleteUserEvent(user_id: number, event_id: number, callback: function){
+    deleteUserEvent(user_id, event_id, callback){
         super.query(
             "DELETE FROM user_event WHERE user_id = ? AND event_id = ?",
             [user_id,event_id], callback
@@ -53,7 +52,7 @@ module.exports = class userEventDao extends Dao{
         @parameter {number} accepted - et tall som beskriver om brukeren har godtatt (1), avslått (0), eller ikke svart på om de kan ta vakta (alt annet).
         @parameter {function} callback - funksjon som returner data fra kallet
      */
-    updateUserEvent(user_id: number, event_id: number, json:{user_id: number, job_position: string, accepted: number}, callback: function){
+    updateUserEvent(user_id, event_id, json, callback){
         super.query(
             "UPDATE user_event SET user_id = ?, job_position = ?, accepted=? WHERE user_id = ? AND event_id = ?",
             [json.user_id, json.job_position,json.accepted, user_id, event_id], callback
